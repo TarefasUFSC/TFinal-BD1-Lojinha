af5e262c5ce52623638e77b4b623f39cc27fb5b0
ManoellaR <manu.rockemba@gmail.com>
2021-08-28 19:44:55 -0300

    populei a tabela extrato cliente

--- Backend/src/database/seeds/02_extrato.js
+++ Backend/src/database/seeds/02_extrato.js
@@ -8,6 +8,21 @@ exports.seed = function(knex) {
       return knex('ExtratoCliente').insert([
         {Data:parseInt(new Date(2021,07,05).getTime()/1000),id_Cliente:1,Movimentacao:200},
         {Data:parseInt(new Date(2021,07,04).getTime()/1000),id_Cliente:1,Movimentacao:-300},
-      ]);
+        {Data:parseInt(new Date(2021,07,01).getTime()/1000),id_Cliente:1,Movimentacao:100},
+        {Data:parseInt(new Date(2021,06,25).getTime()/1000),id_Cliente:2,Movimentacao:150},
+        {Data:parseInt(new Date(2021,06,30).getTime()/1000),id_Cliente:2,Movimentacao:300},
+        {Data:parseInt(new Date(2021,07,12).getTime()/1000),id_Cliente:2,Movimentacao:25},
+        {Data:parseInt(new Date(2021,05,25).getTime()/1000),id_Cliente:3,Movimentacao:100},
+        {Data:parseInt(new Date(2021,05,30).getTime()/1000),id_Cliente:3,Movimentacao:-100},
+        {Data:parseInt(new Date(2021,06,30).getTime()/1000),id_Cliente:3,Movimentacao:30},
+        {Data:parseInt(new Date(2021,07,10).getTime()/1000),id_Cliente:4,Movimentacao:-10},
+        {Data:parseInt(new Date(2021,07,11).getTime()/1000),id_Cliente:4,Movimentacao:25},
+        {Data:parseInt(new Date(2021,07,12).getTime()/1000),id_Cliente:4,Movimentacao:-5},
+        {Data:parseInt(new Date(2021,06,26).getTime()/1000),id_Cliente:5,Movimentacao:-15},
+        {Data:parseInt(new Date(2021,07,20).getTime()/1000),id_Cliente:5,Movimentacao:60},
+        {Data:parseInt(new Date(2021,07,26).getTime()/1000),id_Cliente:5,Movimentacao:-45},
+        {Data:parseInt(new Date(2021,04,29).getTime()/1000),id_Cliente:6,Movimentacao:100},
+        {Data:parseInt(new Date(2021,05,15).getTime()/1000),id_Cliente:6,Movimentacao:-50},
+        {Data:parseInt(new Date(2021,06,23).getTime()/1000),id_Cliente:6,Movimentacao:-50},
     });
 };
