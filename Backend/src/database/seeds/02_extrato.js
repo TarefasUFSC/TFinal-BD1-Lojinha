9117fc355812497e026342b3de9c66802829bfe2
ManoellaR <manu.rockemba@gmail.com>
2021-08-28 19:27:39 -0300

    extrato cliente

--- Backend/src/database/seeds/02_extrato.js
+++ Backend/src/database/seeds/02_extrato.js
@@ -1,12 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('Extrato').del()
+  return knex('ExtratoCliente').del()
     .then(function () {
       // Inserts seed entries
       //Janeiro é mes 0 no Date()
-      return knex('Extrato').insert([
+      return knex('ExtratoCliente').insert([
         {Data:parseInt(new Date(2021,07,05).getTime()/1000),id_Cliente:1,Movimentacao:200},
+        {Data:parseInt(new Date(2021,07,04).getTime()/1000),id_Cliente:1,Movimentacao:300},
       ]);
     });
 };
