e0d0c7e10f04f1807aacf2bd0bc5a376172e5843
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-08-28 18:48:23 -0300

    editei as seeds pra me deixar rodar

--- Backend/src/database/seeds/04_contato.js
+++ Backend/src/database/seeds/04_contato.js
@@ -1,13 +1,11 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('Contato').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('Contato').insert([
+        //{id_Fornecedor: 1, colName: 'rowValue1'},
       ]);
     });
 };
