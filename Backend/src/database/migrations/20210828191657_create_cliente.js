5260708aeda56b1cb3bf023abc5da252ea52ecb2
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-08-28 16:32:01 -0300

    criei os arquivos de todas as migrations

--- Backend/src/database/migrations/20210828191657_create_cliente.js
+++ Backend/src/database/migrations/20210828191657_create_cliente.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
       table.integer("id_Cliente").primary();
       table.string("Estado").notNullable();
       table.string("CEP",20).notNullable();    
-      table.string("Cidade", 50).notNullable(); 
+      table.string("Cidade", 58).notNullable(); 
       table.string("Endereco",150).notNullable();   
       table.float("Saldo").notNullable();   
       table.string("ImagemPerfil").notNullable();   
