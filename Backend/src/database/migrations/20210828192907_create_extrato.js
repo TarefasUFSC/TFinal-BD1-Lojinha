5a066c20ed737aa6f77005fbf0daeb707227ca60
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-08-28 16:42:45 -0300

    criei a table extrato

--- Backend/src/database/migrations/20210828192907_create_extrato.js
+++ Backend/src/database/migrations/20210828192907_create_extrato.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('Extrato', function(table){
+        table.increments("id_Extrato").primary();
+        table.timestamp("Data").notNullable();
+        table.foreign('id_Cliente')
+            .references('id_Cliente')
+            .inTable('Cliente')   
+        table.decimal("Movimentação", 58).notNullable(); 
   
-};
-
-exports.down = function(knex) {
+    })
+  };
   
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTable('Extrato');
+  };
+  
