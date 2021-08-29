be8fb81f144e33c0cc35f45af634226cdb0d0ecf
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2021-08-29 02:30:43 +0000

    alterei o get do extrato pra header

--- Backend/src/controllers/ExtratoClienteController.js
+++ Backend/src/controllers/ExtratoClienteController.js
@@ -3,7 +3,7 @@ const connection = require("../database/connection");
 
 module.exports = {
     async getAllExtByID(req,res){
-        const {id_cliente} = req.body;
+        const {id_cliente} = req.headers;
         const ext = await connection("ExtratoCliente").select("*").where("id_Cliente",id_cliente);
         return res.json({"response":ext})
     }
