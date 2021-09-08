b90951e5216fec77350c9df9255d54ddbecc8272
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-09-08 08:51:36 -0300

    adicionei os limites do periodo do extrato

--- Backend/src/controllers/User/ExtratoClienteController.js
+++ Backend/src/controllers/User/ExtratoClienteController.js
@@ -5,6 +5,7 @@ module.exports = {
     async getAllExtByID(req, res) {
         const { id } = req.params;
         const { reqid, reqtype } = req.headers;
+        const { inicio, fim } = req.body;
         if (parseInt(reqid) != parseInt(id)) {
 
             return res.status(400).json({ "Erro": "Você não tem autorização para ver isso" })
@@ -13,13 +14,17 @@ module.exports = {
 
             return res.status(400).json({ "Erro": "Header incorreto" })
         }
+        if (!inicio || !fim) {
+
+            return res.status(400).json({ "Erro": "Periodo incorreto" })
+        }
         let ext;
         if (reqtype == "0") {
 
-            ext = await connection("ExtratoCliente").select("*").where("id_Cliente", id);
+            ext = await connection("ExtratoCliente").select("*").where("id_Cliente", id).whereBetween("Data", [inicio, fim]);
         } else {
 
-            ext = await connection("ExtratoFornecedor").select("*").where("id_Fornecedor", id);
+            ext = await connection("ExtratoFornecedor").select("*").where("id_Fornecedor", id).whereBetween("Data", [inicio, fim]);
         }
         if (!ext.length) {
             return res.status(404).json({ "Erro": "Nenhum extrato encontrado" })
