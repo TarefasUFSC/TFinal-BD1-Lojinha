062ea93945bc792152c676d1bc77dd6750cf582c
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-09-11 19:25:35 -0300

    .

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const routes = require('./routes')
+const bodyParser = require("body-parser");
+const routes = require('./routes');
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
 app.use(routes)
-app.use(express.json({ limit: '5000mb' }));
-app.use(express.urlencoded({ limit: "5000mb" }));
 app.listen(3333);
