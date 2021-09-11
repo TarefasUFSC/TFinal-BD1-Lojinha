65a140cc5fcbb14395b4f5b48c0b67be80b53996
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-09-11 19:10:01 -0300

    limit size

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,5 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(routes)
-
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.listen(3333);
