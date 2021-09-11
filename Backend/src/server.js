caa0ac040e44527bba23434eb3dd3d7496eaf901
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-09-11 19:22:11 -0300

    .

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(routes)
-app.use(express.json({ limit: '5000000mb' }));
-app.use(express.urlencoded({ limit: "5000000mb" }));
+app.use(express.json({ limit: '5000mb' }));
+app.use(express.urlencoded({ limit: "5000mb" }));
 app.listen(3333);
