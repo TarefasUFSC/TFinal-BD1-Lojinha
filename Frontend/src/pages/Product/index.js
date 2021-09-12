478f2e5fbea2654f2cdb80665d569ed0f049f51f
Perna <engcomp.lucasedu@gmail.com>
2021-09-12 09:30:52 -0300

    Pagina do item reativa (falta comentarios)

--- Frontend/src/pages/Product/index.js
+++ Frontend/src/pages/Product/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { Link, useHistory } from "react-router-dom";
 import api from "../../services/api";
 
@@ -8,34 +8,57 @@ import Header from "../Header";
 import "./styles.css";
 
 export default function Produto(props) {
+  const id = props.match.params.id;
+  const [titulo, setTitulo] = useState('');
+  const [nota, setNota] = useState('');
+  const [preco, setPreco] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [img, setImg] = useState('');
   let [qtd, setQtd] = useState(1);
+  let [estoque, setEstoque] = useState();
   
+  useEffect(async () => {
+    await api.get("/produto/"+String(id), {}).then((response) => {
+      setEstoque(response.data.Detalhes[0].Quantidade);
+      setTitulo(response.data.Detalhes[0].Nome);
+      setNota(response.data.Detalhes[0].media);
+      setPreco(response.data.Detalhes[0].Valor);
+      setDescricao(response.data.Detalhes[0].Descricao);
+      setImg(response.data.Detalhes[0].Imagem);
+    });
+  }, []);
+
   function handleIncrease(){
-      setQtd(qtd+1);
+    if(qtd<estoque) setQtd(qtd+1);
   }
   function handleDecrease(){
       if(qtd!=1) setQtd(qtd-1);
   }
 
-  const id = props.match.params.id;
   return (
     <div className="produto-container">
       <Header />
       <div className="produto-info-container">
         <div className="produto-img-desc-container">
           <div className="produto-img-container">
-            <img className="produto-img" src={logoImg} />
+            <img className="produto-img"
+                src={
+                    img
+                      ? "data:image/png;base64," + img
+                      : logoImg
+                  }
+                />
           </div>
           <div className="produto-desc">
-            <p>TESTE</p>
+            <p>{descricao}</p>
           </div>
         </div>
         <div className="produto-valor-botoes-container">
           <div className="produto-titulo">
-            <p><b>Titulo</b></p>
+            <p><b>{titulo}</b></p>
           </div>
           <div className="produto-preco">
-            <p>Preco</p>
+            <p>Preço: R$ {preco}</p>
           </div>
           <div className="produto-qtd-container">
             <div className="produto-qtd">
@@ -50,7 +73,7 @@ export default function Produto(props) {
             <button className="produto-comprar-button">Comprar</button>
           </div>
           <div className="produto-nota">
-            <p>NOTA 0</p>
+            <p>Nota: {nota}</p>
           </div>
         </div>
       </div>
--- Frontend/src/pages/listProdItem/index.js
+++ Frontend/src/pages/listProdItem/index.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 
 import "./styles.css";
-import api from "../../services/api";
 
 import logoImg from "../../assets/logo.png";
 
