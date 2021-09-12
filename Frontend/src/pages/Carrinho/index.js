89cdd473f132a4362f9f7a37b0284271e8ef15ff
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2021-09-12 22:25:45 +0000

    inicio do carrinho dinamico

--- Frontend/src/pages/Carrinho/index.js
+++ Frontend/src/pages/Carrinho/index.js
@@ -9,10 +9,34 @@ import "./styles.css";
 
 export default function Carrinho(props) {
   const [carrinho, setCarrinho] = useState([]);
+  const [itens, setItens] = useState([]);
+  const [total, setTotal] = useState();
   useEffect(async () => {
     getCarrinho();
   }, []);
-  async function getCarrinho(id, qtd) {
+  async function getProduto(id) {
+    try {
+      const response = await api.get("/produto/" + String(id), {});
+      return response.data.Detalhes[0];
+    } catch (err) {
+      return null;
+    }
+  }
+  useEffect(async () => {
+    let its = [];
+    for (let i in carrinho) {
+      console.log(carrinho[i][0]);
+      let it = await getProduto(carrinho[i][0]);
+      its.push([it, carrinho[i][1]]);
+    }
+    await setItens(its);
+    console.log(its);
+  }, [carrinho]);
+  useEffect(async () => {
+    getTotal();
+  }, [itens]);
+
+  async function getCarrinho() {
     let carr = await localStorage.getItem("carrinho");
 
     if (carr) {
@@ -25,7 +49,7 @@ export default function Carrinho(props) {
         await auxCar.push(itCar);
       }
       await console.log(auxCar);
-      
+
       await setCarrinho(auxCar);
     }
     console.log(carrinho);
@@ -36,50 +60,94 @@ export default function Carrinho(props) {
   function handleDecrease() {
     //if (qtd != 1) setQtd(qtd - 1);
   }
+  async function getTotal() {
+    let tot = 0;
+    for (let i in itens) {
+      let it = itens[i];
+      tot = tot + parseFloat(it[0].Valor) * parseInt(it[1]);
+      console.log(tot);
+    }
+    console.log(tot);
+    setTotal(parseInt(tot * 100) / 100);
+  }
   return (
     <div className="carrinho-container">
       <Header />
       <div className="carrinho-content-container">
         <div className="carrinho-itens-container">
           <div className="carrinho-title-container">
-            <p>Carrinho</p>
+            <p>
+              <b>Carrinho</b>
+            </p>
           </div>
-          {carrinho ? carrinho.map((it) => <div className="carrinho-itemdata-container">
-            <div className="carrinho-item-img-container">
-              <img className="carrinho-item-img" src={logoImg}></img>
-            </div>
-            <div className="item-name-container">
-              <p>Nome</p>
-            </div>
-            <div className="item-stats-container">
-              <div className="produto-qtd-container">
-                <div className="produto-qtd">
-                  <p>Quantidade: {}</p>
+          {itens ? (
+            itens.map((it) => (
+              <div className="carrinho-itemdata-container">
+                <div className="carrinho-item-img-container">
+                  <img
+                    className="carrinho-item-img"
+                    src={
+                      it[0].Imagem
+                        ? "data:image/png;base64," + it[0].Imagem
+                        : logoImg
+                    }
+                  ></img>
                 </div>
-                <div className="produto-botoes">
-                  <button
-                    onClick={handleIncrease}
-                    className="produto-qtd-button"
-                  >
-                    +
-                  </button>
-                  <button
-                    onClick={handleDecrease}
-                    className="produto-qtd-button"
-                  >
-                    -
-                  </button>
+                <div className="item-name-container">
+                  <p>{it[0].Nome}</p>
+                </div>
+                <div className="item-stats-container">
+                  <div className="produto-qtd-container">
+                    <div className="produto-qtd">
+                      <p>Quantidade: {it[1]}</p>
+                    </div>
+                    <div className="produto-botoes">
+                      <button
+                        onClick={handleIncrease}
+                        className="produto-qtd-button"
+                      >
+                        +
+                      </button>
+                      <button
+                        onClick={handleDecrease}
+                        className="produto-qtd-button"
+                      >
+                        -
+                      </button>
+                    </div>
+                  </div>
+                  <div className="carrinho-itempreco-container">
+                    <p>Preco: R${it[0].Valor}</p>
+                  </div>
                 </div>
               </div>
-              <div className="carrinho-itempreco-container">
-                <p>Preco</p>
-              </div>
-            </div>
-          </div>
-        ) : <p>Vazio</p>}
+            ))
+          ) : (
+            <p>Vazio</p>
+          )}
         </div>
         <div className="carrinho-resumo-container">
-          <p>ok</p>
+          <div className="carrinho-dados-resumo">
+            <div className="carrinho-title-container">
+              <p>Resumo</p>
+            </div>
+            <div className="carrinho-itens-preco-final">
+              {itens ? (
+                itens.map((it) => (
+                  <div className="it-preco">
+                    <p>{it[0].Nome}</p>
+                    <p>R${parseFloat(it[0].Valor) * parseInt(it[1])}</p>
+                  </div>
+                ))
+              ) : (
+                <p>Vazio</p>
+              )}
+            </div>
+            <div className="carrinho-total-container">
+              <p>Total: R${total ? total : "0"}</p>
+            </div>
+          </div>
+            <div className="resumo-complemento"></div>
         </div>
       </div>
     </div>
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://3333-amaranth-marten-q30fzc22.ws-us15.gitpod.io', 
+    
+    baseURL: 'https://3333-amethyst-elephant-b61wqaa6.ws-us15.gitpod.io', 
 })
 
 export default api;
